659dbb10c40dd3e61109524262012cc8ff3e7af8
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-18 15:36:36 -0600

    defined Product model, created associations between Category and Product

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: Datatype.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: Datatype.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Datatype.DECIMAL,
+      allowNull:false,
+      validate: {
+        isString: true
+      }
+    },
+    stock: {
+      type: Datatype.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: Datatype.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+
   },
   {
     sequelize,
